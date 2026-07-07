2bdc7b0df27d6fc78e967c0652199e50c26d0bb0
Mayur Mohite <mayurmohite2610@gmail.com>
2026-07-07 19:01:53 +0530

    Create Insert_Data.sql

---
+++ Sql/Insert_Data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Ecommerce_Sales VALUES
+(101, DATE '2025-01-10', 'Amit', 'Laptop', 'Electronics', 'North', 1, 55000);
+
+INSERT INTO Ecommerce_Sales VALUES
+(102, DATE '2025-01-15', 'Priya', 'Mobile', 'Electronics', 'South', 2, 40000);
+
+INSERT INTO Ecommerce_Sales VALUES
+(103, DATE '2025-02-01', 'Rahul', 'Headphones', 'Accessories', 'West', 3, 9000);
+
+INSERT INTO Ecommerce_Sales VALUES
+(104, DATE '2025-02-12', 'Sneha', 'Keyboard', 'Accessories', 'East', 2, 3000);
+
+INSERT INTO Ecommerce_Sales VALUES
+(105, DATE '2025-03-05', 'Kiran', 'Monitor', 'Electronics', 'North', 1, 18000);
+
+COMMIT;
